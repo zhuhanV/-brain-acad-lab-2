45eeee1311b6404c736695fe4e08b434334baa2c
Vira <o2898765@ukr.net>
2022-07-12 13:37:41 +0300

    new lab

---
+++ .idea/src/ua/zp/brainacad/ItemOne.java
@@ -0,0 +1,15 @@
+package ua.zp.brainacad;
+
+import java.util.Arrays;
+
+/**
+ * This is simple Class
+ */
+
+
+public class ItemOne {
+ public static void main(String[] args) {
+ System.out.println("This is first app");
+
+ }
+}
---
+++ .idea/src/ua/zp/brainacad/ItemTwo.java
@@ -0,0 +1,19 @@
+package ua.zp.brainacad;
+
+import java.util.Arrays;
+/**
+ * This is simple Class
+ */
+
+
+public class ItemTwo {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Vira");
+        System.out.println("Hello"+'\t'+"Hello"+'\t'+"Hello"+'\t'+"Hello");
+        System.out.println("W"+'\n'+"o"+'\n'+"r"+'\n'+"d");
+
+
+
+    }
+}
